88db0f93c458d06ecc8326ca21d2d921f396f646
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-29 18:35:11 -0300

    Patience: Corrige validação das ancoras.
    - Validação com a função VEC.contains() -> O(n.m)
    - Validação com a função HASHSET.contains() -> O(1)
    - Corrige split do conteúdo do arquivo, atendendo outras formas quebras de linhas.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashSet}, fs};
+use std::{collections::{HashMap, HashSet}, fs};
 
 fn main() {
     let file_a = "src/fileA.txt";
@@ -10,13 +10,22 @@ fn main() {
     let content_b = fs::read_to_string(file_b)
         .expect("Ocorreu um erro ao ler o FileB");
 
-    let content_lines_a: Vec<&str> = content_a.split("\n").collect();
-    let content_lines_b: Vec<&str> = content_b.split("\n").collect();
+    let content_lines_a: Vec<&str> = content_a.lines().collect();
+    let content_lines_b: Vec<&str> = content_b.lines().collect();
 
-    let mut unique_lines_a: Vec<&str> = get_unique_lines(content_lines_a);
-    let mut unique_lines_b: Vec<&str> = get_unique_lines(content_lines_b);
+    let unique_lines_a: Vec<&str> = get_unique_lines(&content_lines_a);
+    let unique_lines_b: Vec<&str> = get_unique_lines(&content_lines_b);
+
+    let mut anchors = Vec::new();
+
+    let set_b: HashSet<_> = unique_lines_b.iter().cloned().collect();
+    for &line_a in &unique_lines_a {
+        if set_b.contains(line_a) {
+            anchors.push(line_a);
+    }
 }
 
+}
 
 fn get_unique_lines<'a>(content_lines: &'a [&'a str]) -> Vec<&'a str> {
     let mut freq: HashMap<&'a str, usize> = HashMap::new();
