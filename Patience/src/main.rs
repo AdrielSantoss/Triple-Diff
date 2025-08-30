56e3f17ad98945fad97d54d00a2f499713663eca
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-30 14:21:06 -0300

    WIP/Patience: Encontra a subsequencia comum mais longa e ordenada entre dois arquivos.
    - Esta funcional, mas não esta completo e otimizado.

--- Patience/src/main.rs
+++ Patience/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet}, fs, ops::Index};
+use std::{collections::{HashMap, HashSet}, fs};
 
 fn main() {
     let file_a = "src/fileA.txt";
@@ -16,13 +16,49 @@ fn main() {
     let unique_lines_a: HashMap<&str, usize> = get_unique_lines(&content_lines_a);
     let unique_lines_b: HashMap<&str, usize> = get_unique_lines(&content_lines_b);
 
-    let mut anchors: Vec<(&str, usize)> = Vec::new();
+    let mut anchors: Vec<&str> = Vec::new();
+    let mut positions_b: Vec<usize> = Vec::new();
 
     for &line in content_lines_a.iter() {
         if let Some(&pos_b) = unique_lines_b.get(line) {
             if unique_lines_a.contains_key(line) {
-                anchors.push((line, pos_b));
+                anchors.push(line);
+                positions_b.push(pos_b);
             }
+        }
+    }
+
+    let mut anchorsFinal: Vec<&str> = Vec::new();
+    let mut lastIndex: i32 = -1;
+
+    for (position_a, line_a) in content_lines_a.iter().enumerate() {
+        let mut anchorPos: usize = 0;
+
+        if !anchors.contains(line_a) {
+            continue;
+        }
+        else {
+            anchorPos = anchors.iter().position(|x| x == line_a).unwrap();
+        }
+
+        let position_b = positions_b[anchorPos];
+        
+        if lastIndex == -1 {
+            if position_b > position_a {
+                continue;
+            }
+            else {
+                anchorsFinal.push(line_a);
+                lastIndex = position_a as i32;
+                continue;
+            }   
+        }
+
+        let lastIndexUsized: usize = lastIndex as usize;
+
+        if position_b > lastIndexUsized && position_b >= position_a {
+            anchorsFinal.push(line_a);
+            lastIndex = position_a as i32;
         }
     }
 }
