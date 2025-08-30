1e9b3bac5a169c136a3006433943d2523d460608
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-30 11:40:27 -0300

    Patience: Melhora desempenho do diff, com HASHMAPS e VETOR com tuplas.
    - get_unique_lines(): HashMap<str, usize>: linha, posicao
    - anchors: Vec<(str, usize)>: linha, posicao
    - não é ideal anchors ser um hashmap, hashmaps nao garantem ordenação, vetore garantem.

--- Patience/src/main.rs
+++ Patience/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet}, fs};
+use std::{collections::{HashMap, HashSet}, fs, ops::Index};
 
 fn main() {
     let file_a = "src/fileA.txt";
@@ -13,30 +13,34 @@ fn main() {
     let content_lines_a: Vec<&str> = content_a.lines().collect();
     let content_lines_b: Vec<&str> = content_b.lines().collect();
 
-    let unique_lines_a: Vec<&str> = get_unique_lines(&content_lines_a);
-    let unique_lines_b: Vec<&str> = get_unique_lines(&content_lines_b);
+    let unique_lines_a: HashMap<&str, usize> = get_unique_lines(&content_lines_a);
+    let unique_lines_b: HashMap<&str, usize> = get_unique_lines(&content_lines_b);
 
-    let mut anchors = Vec::new();
+    let mut anchors: Vec<(&str, usize)> = Vec::new();
 
-    let set_b: HashSet<_> = unique_lines_b.iter().cloned().collect();
-    for &line_a in &unique_lines_a {
-        if set_b.contains(line_a) {
-            anchors.push(line_a);
+    for &line in content_lines_a.iter() {
+        if let Some(&pos_b) = unique_lines_b.get(line) {
+            if unique_lines_a.contains_key(line) {
+                anchors.push((line, pos_b));
+            }
+        }
     }
 }
 
-}
-
-fn get_unique_lines<'a>(content_lines: &'a [&'a str]) -> Vec<&'a str> {
+fn get_unique_lines<'a>(content_lines: &'a [&'a str]) -> HashMap<&'a str, usize> {
     let mut freq: HashMap<&'a str, usize> = HashMap::new();
+    let mut result: HashMap<&'a str, usize> = HashMap::new();
 
-    for &line in content_lines.iter() {
-        *freq.entry(line).or_insert(0) += 1;
+    for (i, &line) in content_lines.iter().enumerate() {    
+        let count = freq.entry(line).or_insert(0);
+        *count += 1;
+
+        if *count == 1 {
+            result.insert(line, i);
+        } else {
+            result.remove(line);
+        }
     }
 
-    return content_lines
-        .iter()
-        .filter(|&&line| freq.get(line) == Some(&1))
-        .map(|&line| line)
-        .collect()
+    return result;
 }
