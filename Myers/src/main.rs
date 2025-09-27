8c157194b26d630efe3699271dcbeeb0436d2adb
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-26 21:28:56 -0300

    Myers: Read files via the command line.

--- Myers/src/main.rs
+++ Myers/src/main.rs
@@ -1,21 +1,33 @@
-use std::fs;
+use std::{env, fs};
 use myers::{myers_diff, write_patch_file};
 
 fn main() {
-    let file_a = "src/fileA.txt";
-    let file_b = "src/fileB.txt";
+    // Captura argumentos da linha de comando
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <fileA> <fileB>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_a = &args[1];
+    let file_b = &args[2];
 
     let content_a = fs::read_to_string(file_a)
-        .expect("Ocorreu um erro ao ler o FileA");
+        .expect(&format!("Error reading file {}", file_a));
 
     let content_b = fs::read_to_string(file_b)
-        .expect("Ocorreu um erro ao ler o FileB");
+        .expect(&format!("Error reading file {}", file_b));
 
     let lines_a: Vec<&str> = content_a.lines().collect();
     let lines_b: Vec<&str> = content_b.lines().collect();
 
     let edits = myers_diff(&lines_a, &lines_b);
 
-    write_patch_file(&edits, "patch.diff");
+    if edits.is_empty() {
+        println!("No differences found");
+    } else {
+        write_patch_file(&edits, "patch.diff");
+        println!("Diff written to patch.diff");
+    }
 }
-
